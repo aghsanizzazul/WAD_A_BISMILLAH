a9ae16ad01ed85069c2042c4976e5a547eafee23
Ijul <aharryplos89@gmail.com>
2025-06-06 14:20:01 +0700

    fix API

--- WAD_BISMILLAH_A/app/Http/Controllers/Api/PembayaranApiController.php
+++ WAD_BISMILLAH_A/app/Http/Controllers/Api/PembayaranApiController.php
@@ -2,6 +2,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pembayaran;
+use Illuminate\Http\Request;
 
 class PembayaranApiController extends Controller
 {
--- WAD_BISMILLAH_A/routes/api.php
+++ WAD_BISMILLAH_A/routes/api.php
@@ -1,24 +1,40 @@
-use App\Http\Controllers\Api\PembayaranApiController;
-
-Route::get('/pembayaran', [PembayaranApiController::class, 'index']); // semua data
-Route::get('/pembayaran/{id}', [PembayaranApiController::class, 'show']); // 1 data
 <?php
 
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrainerController;
+use App\Http\Controllers\Api\PembayaranApiController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KelasController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    route::get('/', [AuthController::class, 'index']);
-    route::get('/register', [AuthController::class, 'register']);
-    route::post('/store', [AuthController::class, 'store']);
-    route::post('/login', [AuthController::class, 'login']);
-    route::get('/kelas', [KelasController::class, 'index']);
-    route::get('/kelas/create', [KelasController::class, 'create']);
-    route::post('/kelas', [KelasController::class, 'store']);
-    route::get('/kelas/{id}', [KelasController::class, 'show']);
-    route::put('/kelas/{id}', [KelasController::class, 'update']);
-    route::delete('/kelas/{id}', [KelasController::class, 'destroy']);
-    return $request->user();
+// Public API routes
+Route::middleware('api')->group(function () {
+    // Pelatih API (Fikri)
+    Route::apiResource('pelatih', TrainerController::class);
+
+    // Pembayaran API (Imam & Ijul)
+    Route::get('/pembayaran', [PembayaranApiController::class, 'index']);
+    Route::get('/pembayaran/{id}', [PembayaranApiController::class, 'show']);
+});
+
+// Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    // User data
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // AuthController routes (Aceng & Imam)
+    Route::get('/', [AuthController::class, 'index']);
+    Route::get('/register', [AuthController::class, 'register']);
+    Route::post('/store', [AuthController::class, 'store']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    // KelasController routes (Aceng & Imam)
+    Route::get('/kelas', [KelasController::class, 'index']);
+    Route::get('/kelas/create', [KelasController::class, 'create']);
+    Route::post('/kelas', [KelasController::class, 'store']);
+    Route::get('/kelas/{id}', [KelasController::class, 'show']);
+    Route::put('/kelas/{id}', [KelasController::class, 'update']);
+    Route::delete('/kelas/{id}', [KelasController::class, 'destroy']);
 });
